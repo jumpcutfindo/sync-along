85db4802e1b98f85a3b2e0d9cb0167e3bc665473
Chow Jia Ying <chowjiaying211@gmail.com>
2021-09-22 17:40:20 +0800

    Add components

---
+++ frontend/src/api/axiosConfig.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8080/';
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default axiosInstance;
