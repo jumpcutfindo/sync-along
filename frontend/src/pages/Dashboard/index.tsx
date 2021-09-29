a6eefc110c51dd8e34a75e01326c25ac0edbce9e
jumpcutfindo <danielhoe97@gmail.com>
2021-09-29 21:01:01 +0800

    Initial setup tweaks and adjustments

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DashboardPage: React.FC = () => {
-    return <div>This is a dashboard</div>;
+    return <div className="Dashboard">This is a dashboard</div>;
 };
 
 export default DashboardPage;
