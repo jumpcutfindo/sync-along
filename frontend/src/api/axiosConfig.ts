0f56aeaf288f9a6ec533cf6526ddac9bf17063b2
Chow Jia Ying <chowjiaying211@gmail.com>
2021-10-07 19:43:16 +0800

    Remove axios

--- frontend/src/api/axiosConfig.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:8080/";
-const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-});
-
-export default axiosInstance;
