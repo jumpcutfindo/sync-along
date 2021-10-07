d200c98e024486bbd886f5b4a5a4f9452c5cbc43
Chow Jia Ying <chowjiaying211@gmail.com>
2021-10-07 19:05:34 +0800

    Add types to user API

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,10 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+interface AuthResponse {
+    accessToken?: string;
+}
+
+interface LoginParams {
+    username: string;
+    password: string;
+}
+
 const userApi = createApi({
     reducerPath: "userApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000" }),
     endpoints: (builder) => ({
-        login: builder.mutation({
+        login: builder.mutation<AuthResponse, LoginParams>({
             query: (body) => ({
                 url: "/login",
                 method: "POST",
