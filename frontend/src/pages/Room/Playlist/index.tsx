0539c5af856a793cb35aab9b4d3ebb58fd12a964
jumpcutfindo <danielhoe97@gmail.com>
2021-10-11 13:45:17 +0800

    Create playlist header component

--- frontend/src/pages/Room/Playlist/index.tsx
+++ frontend/src/pages/Room/Playlist/index.tsx
@@ -2,8 +2,34 @@ import React from "react";
 
 import "./index.css";
 
+const PlaylistHeader: React.FC = () => (
+    <div className="PlaylistHeader d-flex mb-2">
+        <div className="d-flex w-100 m-3">
+            <h2 className="m-0">PLAYLISTS</h2>
+            <div className="PlaylistHeaderButtons d-flex w-100 justify-content-end">
+                <button type="button" className="btn btn-primary my-auto me-2">
+                    Add a Song
+                </button>
+                <button type="button" className="btn btn-success my-auto">
+                    Import Playlist
+                </button>
+            </div>
+        </div>
+    </div>
+);
+
+const Playlist: React.FC = () => {
+    return <div className="Playlist">This is the main playlist</div>;
+};
+
 const PlaylistComponent: React.FC = () => {
-    return <div className="PlaylistComponent h-100">This is the playlist</div>;
+    return (
+        <div className="PlaylistComponent d-flex-column h-100">
+            <PlaylistHeader />
+            <hr className="my-0" />
+            <Playlist />
+        </div>
+    );
 };
 
 export default PlaylistComponent;
