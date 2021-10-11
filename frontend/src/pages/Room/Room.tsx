0924c1ffedbfad7a05ee01592692379f740d61ad
jumpcutfindo <danielhoe97@gmail.com>
2021-10-11 12:57:24 +0800

    Change room structure to follow dashboard

---
+++ frontend/src/pages/Room/Room.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import ChatComponent from "./Chat";
+import PlayerComponent from "./Player";
+import PlaylistComponent from "./Playlist";
+import RoomInfoComponent from "./RoomInfo";
+
+import "./Room.css";
+
+const RoomScreen: React.FC = () => {
+    return (
+        <div className="RoomScreen d-flex w-100 h-100">
+            <div className="PlayerColumn d-flex-column h-100">
+                <div className="PlaylistComponentContainer">
+                    <PlaylistComponent />
+                </div>
+                <div className="PlayerComponentContainer">
+                    <PlayerComponent />
+                </div>
+            </div>
+            <div className="ChatColumn d-flex-column h-100">
+                <div className="RoomInfoComponentContainer">
+                    <RoomInfoComponent />
+                </div>
+                <div className="ChatComponentContainer">
+                    <ChatComponent />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RoomScreen;
--- frontend/src/pages/Room/index.tsx
+++ frontend/src/pages/Room/index.tsx
@@ -1,32 +1,12 @@
 import React from "react";
-import ChatComponent from "./Chat";
-import PlayerComponent from "./Player";
-import PlaylistComponent from "./Playlist";
-import RoomInfoComponent from "./RoomInfo";
+import RoomScreen from "./Room";
 
-import "./index.css";
-
-const RoomScreen: React.FC = () => {
+const RoomPage: React.FC = () => {
     return (
-        <div className="RoomScreen d-flex w-100 h-100 mb-2">
-            <div className="PlayerColumn d-flex-column h-100">
-                <div className="PlaylistComponentContainer">
-                    <PlaylistComponent />
-                </div>
-                <div className="PlayerComponentContainer">
-                    <PlayerComponent />
-                </div>
-            </div>
-            <div className="ChatColumn d-flex-column h-100">
-                <div className="RoomInfoComponentContainer">
-                    <RoomInfoComponent />
-                </div>
-                <div className="ChatComponentContainer">
-                    <ChatComponent />
-                </div>
-            </div>
+        <div className="d-flex w-100 h-100 mb-2">
+            <RoomScreen />
         </div>
     );
 };
 
-export default RoomScreen;
+export default RoomPage;
