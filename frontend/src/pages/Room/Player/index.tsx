8cf06822b3c3b10aeecb9cb55f011bbf4b0f1f0d
jumpcutfindo <danielhoe97@gmail.com>
2021-10-11 17:59:28 +0800

    Add basic player component

--- frontend/src/pages/Room/Player/index.tsx
+++ frontend/src/pages/Room/Player/index.tsx
@@ -1,9 +1,29 @@
+import { SourceInfo } from "plyr";
+import Plyr from "plyr-react";
 import React from "react";
 
 import "./index.css";
+import "./plyr.css";
+
+const Player: React.FC = () => {
+    const source: SourceInfo = {
+        type: "audio",
+        sources: [{ src: "s3Q80mk7bxE", provider: "youtube" }],
+    };
+
+    return (
+        <div className="Player">
+            <Plyr className="my-auto" source={source} />
+        </div>
+    );
+};
 
 const PlayerComponent: React.FC = () => {
-    return <div className="PlayerComponent h-100">This is the player</div>;
+    return (
+        <div className="PlayerComponent h-100">
+            <Player />
+        </div>
+    );
 };
 
 export default PlayerComponent;
