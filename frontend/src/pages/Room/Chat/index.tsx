1dbe385d90c8994111231edc27902601a8b2889a
Chow Jia Ying <chowjiaying211@gmail.com>
2021-10-18 01:01:39 +0800

    Add a messageInput

--- frontend/src/pages/Room/Chat/index.tsx
+++ frontend/src/pages/Room/Chat/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import useInputState from "src/hooks/useInputState";
 
 import "./index.css";
 
+const MessageInput: React.FC = () => {
+    const [messageInput, updateMessageInput] = useInputState("");
+
+    const sendMessage = (event: React.FormEvent) => {
+        event.preventDefault();
+    };
+    return (
+        <form onSubmit={sendMessage}>
+            <input
+                className="form-control"
+                value={messageInput}
+                onChange={updateMessageInput}
+                placeholder="Send a message here!"
+            />
+        </form>
+    );
+};
+
+const MessageList: React.FC = () => {
+    return <div />;
+};
+
 const ChatComponent: React.FC = () => {
-    return <div className="ChatComponent h-100">This is the chat</div>;
+    return (
+        <div className="ChatComponent h-100">
+            <MessageList />
+            <MessageInput />
+        </div>
+    );
 };
 
 export default ChatComponent;
