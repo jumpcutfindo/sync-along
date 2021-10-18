5f04c6b9b5a901ac0af2eeed06bd6076370d11b0
Chow Jia Ying <chowjiaying211@gmail.com>
2021-10-18 11:34:06 +0800

    Add styling for chats

--- frontend/src/pages/Room/Chat/index.tsx
+++ frontend/src/pages/Room/Chat/index.tsx
@@ -21,8 +21,53 @@ const MessageInput: React.FC = () => {
     );
 };
 
+const Message: React.FC<{ user: string; message: string }> = ({
+    user,
+    message,
+}) => {
+    if (user === "User1") {
+        return (
+            <div className="message-container">
+                <div className="text-container is-user">{message}</div>
+            </div>
+        );
+    }
+    return (
+        <div className="message-container">
+            <div className="d-flex">
+                <img
+                    src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+                    alt="user avatar"
+                    width="32"
+                    height="32"
+                />
+                <div className="message text-container">
+                    <div className="username">{user}</div>
+                    <div>{message}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 const MessageList: React.FC = () => {
-    return <div />;
+    const [data] = useState([
+        {
+            user: "User1",
+            message: "New message!",
+        },
+        {
+            user: "User2",
+            message: "Message from other user",
+        },
+    ]);
+    return (
+        <div>
+            {data.map(({ message, user }) => (
+                <Message key="message" user={user} message={message} />
+            ))}
+        </div>
+    );
 };
 
 const ChatComponent: React.FC = () => {
