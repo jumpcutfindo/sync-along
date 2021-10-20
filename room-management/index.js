31f8f2dc2ea76e810ce13b61c4ab2bb85632580c
Dinh Nho Bao <nhobao2000@gmail.com>
2021-10-20 21:55:12 +0800

    Add user state in the application
    - Does not handle the case when one user can be in multiple rooms
    - Does not handle the case when one room can have duplicate users
    - Does not handle the case when the username is invalid from the user DB

--- room-management/index.js
+++ room-management/index.js
@@ -4,7 +4,9 @@ const app = express();
 
 var session = require("express-session");
 
-const rooms = new Map(); // room -> list of users
+var rooms = new Map(); // room -> list of users separated by spaces
+var users = new Map(); // user -> room that the user belongs to
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
@@ -24,12 +26,23 @@ app.get('/', (req, res) => {
 
 /* F3.1 The application should allow users to create a room with a randomly generated room code. */
 app.post('/create', (req, res) => {
+    const username = req.body.username;
+
+    if (!username) {
+        return res.status(400).json({
+            isSuccessful: false,
+            message: "Please provide a username to create a room"
+        })
+    }
+
     while (true) {
         const code = Math.random().toString(36).substr(2, 5).toUpperCase();
     
         if (!rooms.has(code)) { // new room code
-            rooms.set(code, 0);
+            rooms.set(code, username);
+            users.set(username, code);
             console.log(`rooms=${[...rooms.entries()]}`);
+            console.log(`users=${[...users.entries()]}`);
 
             return res.json({
                 code: code,
@@ -40,13 +53,26 @@ app.post('/create', (req, res) => {
 
 /* F3.2 The application should allow users to join a room using a valid room code */
 app.post('/join', (req, res) => {
+    const username = req.body.username;
     const code = req.body.code;
 
+    if (!(username && code)) {
+        return res.status(400).json({
+            isSuccessful: false,
+            message: "Please provide a username and room code to join a room."
+        })
+    }
     if (!rooms.has(code)) {
         return res.status(404).json({
             isSuccessful: false, // room not found
         })
     }
+
+    users.set(username, code);
+    rooms.set(code, rooms.get(code) + "," + username);
+
+    console.log(`rooms=${[...rooms.entries()]}`);
+    console.log(`users=${[...users.entries()]}`);
 
     return res.status(200).json({
         isSuccessful: true,
@@ -62,8 +88,16 @@ app.post("/delete", (req, res) => {
             isSuccessful: false, // room not found to remove
         })
     }
+
+    let room_users = rooms.get(code).split(",");
+    for (let i = 0; i< room_users.length; i++) {
+        console.log(`User ${room_users[i]} from room ${code} removed`);
+        users.delete(room_users[i]);
+    }
     rooms.delete(code);
-    
+
+    console.log(`rooms=${[...rooms.entries()]}`);
+    console.log(`users=${[...users.entries()]}`);
     return res.status(200).json({
         isSuccessful: true,
     })
