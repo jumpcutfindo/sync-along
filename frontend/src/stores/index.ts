205964edce5edc1fa9e2a4f9b60e3ced5862e4f0
Chow Jia Ying <chowjiaying211@gmail.com>
2021-10-25 02:11:19 +0800

    Add a chat service

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -3,10 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // API Slices
 import userApi from "src/services/user";
 import roomApi from "src/services/room";
+import chatApi from "src/services/chat";
 
 // State slices
 import { authReducer, authSlice } from "./auth";
 import { appReducer, appSlice } from "./app";
+import { roomReducer, roomSlice } from "./room";
 import { playlistReducer, playlistSlice } from "./app/playlist";
 import { playerReducer, playerSlice } from "./app/player";
 
@@ -14,10 +16,12 @@ export const store = configureStore({
     reducer: {
         [userApi.reducerPath]: userApi.reducer,
         [roomApi.reducerPath]: roomApi.reducer,
+        [chatApi.reducerPath]: chatApi.reducer,
         [authSlice.name]: authReducer,
         [appSlice.name]: appReducer,
         [playlistSlice.name]: playlistReducer,
         [playerSlice.name]: playerReducer,
+        [roomSlice.name]: roomReducer,
     },
 });
 
