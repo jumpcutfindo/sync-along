56adb019ebb7c88ecc96c4f4b611b9a168bb1422
jumpcutfindo <danielhoe97@gmail.com>
2021-10-28 15:05:10 +0800

    Move useTextAreaState to own file

--- frontend/src/hooks/useTextAreaState.ts
+++ frontend/src/hooks/useTextAreaState.ts
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-const useInputState = (initialState: string) => {
+const useTextAreaState = (initialState: string) => {
     const [input, setInput] = useState(initialState);
-    const updateState = (event: React.ChangeEvent<HTMLInputElement>) =>
+    const updateState = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
         setInput(event.currentTarget.value);
     const clearInput = () => setInput("");
+
     return [input, updateState, clearInput] as const;
 };
 
-export default useInputState;
+export default useTextAreaState;
