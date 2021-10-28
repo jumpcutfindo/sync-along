45a98602375746fb7fce9f09ff066bc5a1497d1a
jumpcutfindo <danielhoe97@gmail.com>
2021-10-28 15:02:44 +0800

    Add textarea handling to useInputState

--- frontend/src/hooks/useInputState.ts
+++ frontend/src/hooks/useInputState.ts
@@ -8,4 +8,13 @@ const useInputState = (initialState: string) => {
     return [input, updateState, clearInput] as const;
 };
 
-export default useInputState;
+const useTextAreaState = (initialState: string) => {
+    const [input, setInput] = useState(initialState);
+    const updateState = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
+        setInput(event.currentTarget.value);
+    const clearInput = () => setInput("");
+
+    return [input, updateState, clearInput] as const;
+};
+
+export default { useInputState, useTextAreaState };
