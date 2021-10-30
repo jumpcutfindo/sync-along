6f9ae0be32b653cac5cca4f79fb5287a0a29820e
jumpcutfindo <danielhoe97@gmail.com>
2021-10-30 15:46:33 +0800

    Move next/prev actions to playlist

--- frontend/src/stores/app/player/actions.ts
+++ frontend/src/stores/app/player/actions.ts
@@ -1,5 +1,3 @@
 export const playAction = "player/play";
 export const pauseAction = "player/pause";
-export const nextAction = "player/next";
-export const prevAction = "player/prev";
 export const scrubAction = "player/scrub";
--- frontend/src/stores/app/playlist/actions.ts
+++ frontend/src/stores/app/playlist/actions.ts
@@ -1,5 +1,7 @@
 export const selectSongAction = "playlist/select";
 export const addSongAction = "playlist/add";
 export const removeSongAction = "playlist/remove";
+export const nextAction = "playlist/next";
+export const prevAction = "playlist/prev";
 
 export const playlistUpdateAction = "playlist/update";
