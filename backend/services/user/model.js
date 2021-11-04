4604f711aa05c33ea3693784928857a2b0bc5ef6
Chow Jia Ying <chowjiaying211@gmail.com>
2021-11-04 10:36:51 +0800

    Add a user model to the user service

---
+++ backend/services/user/model.js
@@ -0,0 +1,25 @@
+/**
+ * Models a User in the database
+ */
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+UserSchema.set('toJSON', {
+  transform: function (doc, ret, options) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
