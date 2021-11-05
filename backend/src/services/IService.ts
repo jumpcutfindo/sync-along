f7057cff85df35cff2fa33b6d778755e51d2bac3
Chow Jia Ying <chowjiaying211@gmail.com>
2021-11-05 15:18:26 +0800

    Remove redundant files

--- backend/src/services/IService.ts
+++
@@ -1,6 +0,0 @@
-import {IO, SocketType} from "../server";
-export interface IServiceController<Service> {
-  new(io: IO, socket: SocketType): Service;
-}
-
-export type StaticImplements<I extends new (...args: any[]) => any, C extends I> = InstanceType<I>;
