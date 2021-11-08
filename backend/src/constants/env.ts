22f3d615ed5521b260221b53347391453209905f
Dinh Nho Bao <nhobao2000@gmail.com>
2021-11-08 16:45:13 +0800

    Add env variables

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/prefer-default-export */
-const FRONTEND_URL = process.env.REDIS_HOST ? `http://frontend-svc:3000` : "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
-const REDIS_HOST = process.env.REDIS_HOST;
-const REDIS_PORT = parseInt(process.env.REDIS_PORT);
-const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+const REDIS_HOST = process.env.REDIS_HOST || "redis-13406.c1.asia-northeast1-1.gce.cloud.redislabs.com";
+const REDIS_PORT = parseInt(process.env.REDIS_PORT) || 13406;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD || "HOBL9WbJWOuNUaFBnTFPBxS49sVFQSyu";
 
-const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL || "mongodb+srv://admin:admin@cluster0.csain.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 export {
     FRONTEND_URL,
