889f9c0148015f60cde51d208ac6817107c9385e
dinhnhobao <nhobao2000@gmail.com>
2021-11-08 06:58:53 +0000

    Update

---
+++ sync-along/scripts.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+echo "helm uninstall chart"
+helm uninstall chart
+echo "cd ~/cs3219-project-ay2122-2122-s1-g6/backend"
+cd ~/cs3219-project-ay2122-2122-s1-g6/backend
+echo "npm install"
+npm install
+echo "tsc"
+tsc
+echo "docker build . -t backend"
+docker build . -t backend
+echo "---Backend built---"
+echo "cd ~/cs3219-project-ay2122-2122-s1-g6/frontend"
+cd ~/cs3219-project-ay2122-2122-s1-g6/frontend
+echo "npm install"
+npm install
+echo "npm run build"
+npm run build
+echo "docker build . -t frontend"
+docker build . -t frontend
+echo "---Frontend built---"
+
+echo "cd ~/cs3219-project-ay2122-2122-s1-g6/sync-along"
+cd ~/cs3219-project-ay2122-2122-s1-g6/sync-along
+echo "helm install chart . -f values.yaml"
+echo "Helm chart built"
