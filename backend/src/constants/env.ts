beb4d7a90c150486151090b88eda5ed71b83419b
Dinh Nho Bao <nhobao2000@gmail.com>
2021-11-08 21:23:28 +0800

    Add localhost

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/prefer-default-export */
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = "http://localhost:3000";
 
 const REDIS_HOST = process.env.REDIS_HOST || "redis-13406.c1.asia-northeast1-1.gce.cloud.redislabs.com";
 const REDIS_PORT = parseInt(process.env.REDIS_PORT) || 13406;
